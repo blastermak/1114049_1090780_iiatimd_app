612fa096e4d28cc9be2d7d32274a69287aa4ef8d
Jim Mak <jim@jimmak.nl>
2021-06-28 21:03:31 +0200

    Put in the correct fields for the basic fields of a recipe, in it's own activity

--- app/src/main/java/com/example/a1114049_1090780_iiatimd_app/newRecipeActivity.java
+++ app/src/main/java/com/example/a1114049_1090780_iiatimd_app/newRecipeActivity.java
@@ -3,12 +3,37 @@ package com.example.a1114049_1090780_iiatimd_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
 
 public class newRecipeActivity extends AppCompatActivity {
+
+    private TextInputEditText recipeTitleText;
+    private TextInputEditText recipeShortDescriptionText;
+    private TextInputEditText recipeDescriptionText;
+    private TextInputEditText recipePrepTimeMin;
+
+    private Button recipeSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_recipe);
+
+        recipeTitleText = findViewById(R.id.editRecipeTitleEditText);
+        recipeShortDescriptionText = findViewById(R.id.newRecipeShortDescriptionEditText);
+        recipeDescriptionText = findViewById(R.id.newRecipeDescriptionEditText);
+        recipePrepTimeMin = findViewById(R.id.newRecipePrepTimeMinEditText);
+
+        recipeSubmitButton = findViewById(R.id.submitNewRecipeButton);
+
+    }
+
+    public void newRecipeSubmitHandler(View view){
+
     }
 }
